a00441c525b056139893a7449119dbd2a6288eaa
iamratedpg13 <pjgranieri@gmail.com>
2025-06-24 15:16:59 +0000

    frontend v1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DetailsPage from './pages/DetailsPage';
+import ItemDetailsPage from './pages/ItemDetailsPage';
+import RelatedStoriesPage from './pages/RelatedStoriesPage';
+
+//funciton for the app
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details" element={<DetailsPage />} />
+        <Route path="/item/:id" element={<ItemDetailsPage />} />
+        <Route path="*" element={<div>Not Found</div>} />
+        <Route path="/related-stories" element={<RelatedStoriesPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
